5701a5ceb7998f3ba1203b59b11ded853807e6e4
Amadou BA <bamadoulife@gmail.com>
2026-07-20 13:57:42 +0100

    Add environment variable for base path

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   output: 'export',
   basePath: '/orchestre-rando',
   assetPrefix: '/orchestre-rando',
+  env: {
+    NEXT_PUBLIC_BASE_PATH: '/orchestre-rando',
+  },
   images: {
     unoptimized: true,
   },
